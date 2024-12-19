fe06001ae713f4910274428aa7abe93300b40acd
David <nozzytb@gmail.com>
2024-12-20 00:07:54 +0100

    Ajout de l'effet de survole sur les onglet tableau de bord et gestion des stages

--- SAE301/accueil-src/script.js
+++ SAE301/accueil-src/script.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Début du JS pour le dernier commit CSS
+
     const navLinks = document.querySelectorAll('header nav ul li a');
 
     navLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault(); // Empêche le comportement par défaut du lien
 
-            const linkText = this.textContent.trim();
+      });
 
-            if (linkText === 'Tableau de bord') {
-                window.location.href = 'tableaudebord.html';
-            } else if (linkText === 'Gestion des stages') {
-                window.location.href = 'gestiondestages.html';
-            }
-        });
+        navLinks.forEach(link => {
+           link.addEventListener('click', function (event) {
+               event.preventDefault();
+              // Effet de survol au click
+                navLinks.forEach(otherLink => otherLink.classList.remove('active'));
+                 this.classList.add('active');
+
+              const linkText = this.textContent.trim();
+
+               if (linkText === 'Tableau de bord') {
+                   window.location.href = 'tableaudebord.html';
+               } else if (linkText === 'Gestion des stages') {
+                   window.location.href = 'gestiondestages.html';
+               }
+           });
     });
-}); // Début du js pour le dernier commit css 
-
+});
