f3efd4038f0e262bf000d0e1efeae87032347db1
moilinho <moilim.abdallah@edu.univ-paris13>
2025-01-03 17:31:07 +0100

    test

--- SAE301/connexion.php
+++ SAE301/connexion.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="connexion-src/style.css">
-    <title>Connexion</title>
+    <title>Connexion aa</title>
 </head>
 <body>
     <div class="container">
