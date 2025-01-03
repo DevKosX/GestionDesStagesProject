ef488549985006c567f5578b4f6d27613c21f413
YACINER7 <rachidi.yacine7@gmail.com>
2025-01-03 16:52:38 +0000

    connexion bdd

--- SAE301/login.php
+++ SAE301/login.php
@@ -4,9 +4,9 @@ session_start();
 
 // Connexion à la base de données
 $host = "localhost";
-$dbname = "nom_de_la_base";
-$username = "nom_utilisateur";
-$password = "mot_de_passe";
+$dbname = "stage";
+$username = "root";
+$password = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
