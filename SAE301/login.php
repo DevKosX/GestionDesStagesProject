a703f2a101445ba5e723161b7db2c9d8dc5a63d9
YACINER7 <rachidi.yacine7@gmail.com>
2025-01-04 21:37:05 +0000

    test

--- SAE301/login.php
+++ SAE301/login.php
@@ -6,11 +6,12 @@ session_start();
 $host = "localhost";
 $dbname = "stage";
 $username = "root";
-$password = "root";
+$password = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "hello";
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
