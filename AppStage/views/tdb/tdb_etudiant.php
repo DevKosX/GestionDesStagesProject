6f4182ef6427e2315fd08ab2ad652ab3fe7c9595
Mohamed Kosbar <kosbarmohamed.31@gmail.com>
2025-01-16 17:32:11 +0100

    modif du conflit

--- AppStage/views/tdb/tdb_etudiant.php
+++ AppStage/views/tdb/tdb_etudiant.php
@@ -37,7 +37,7 @@ try {
         <div class="container">
             <nav>
                 <ul>
-                    <li><a href="../views/accueil.php">Accueil</a></li>
+                    <li><a href="../views/accueilConnect.php">Accueil</a></li>
                     <li><a href="../views/tableaudebord.php" class="active">Tableau de bord</a></li>
                     <li><a href="../views/gestiondestages.php">Gestion des stages</a></li>
                 </ul>
